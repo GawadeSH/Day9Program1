a31848814d7e12c1cd58ea99044f164f8bc439b2
shital gawade <shitalgawade131@gmail.com>
2023-02-06 17:47:17 +0530

    part time wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,12 +4,13 @@ import java.util.Random;
 
 public class EmployeeWageComputation
 {
-    int Attend1;
+    int Attend1,Attend2;
     int EmpRatePerHr,EmpHrs,w;
     EmployeeWageComputation()
     {
         Random r = new Random();
         Attend1 = r.nextInt(2);
+        Attend2 = r.nextInt(2);
     }
     void attendance()
     {
@@ -24,14 +25,20 @@ public class EmployeeWageComputation
     }
     void wage()
     {
-        if(Attend1==1 )
+        if(Attend1==1 && Attend2==1)
         {
             EmpRatePerHr = 20;
             EmpHrs = 8;
             w = EmpRatePerHr * EmpHrs;
-            System.out.println("Employee Wage is :-" + w);
+            System.out.println("FullTime employee and Employee Wage is :-" + w);
         }
-
+        else if (Attend1==1 && Attend2==0)
+        {
+            EmpRatePerHr = 20;
+            EmpHrs = 4;
+            w = EmpRatePerHr * EmpHrs;
+            System.out.println("PartTime employee and Employee Wage is :-" + w);
+        }
         else if(Attend1==0)
         {
             System.out.println("Employee Wage is :-0");
